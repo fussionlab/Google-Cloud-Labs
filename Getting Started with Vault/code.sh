99aec255c337c84ba4c6a3dfd9e250adeb113fef
Radhish Arjunan <fussionlab@users.noreply.github.com>
2025-08-11 16:34:13 +0300

    Create code.sh

---
+++ Getting Started with Vault/code.sh
@@ -0,0 +1,10 @@
+gcloud config list project
+sudo apt update && sudo apt install -y curl gnupg lsb-release
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt-get update
+sudo apt-get install vault
+
+vault
+
+vault server -dev
